1ff06beea1de43429b1fdcec2d4ad19dfbcd70ae
Longtail <endermen359872@icloud.com>
2025-03-05 21:15:01 +0800

    Remove code for demo use.

--- main.py
+++ main.py
@@ -5,16 +5,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# for test only
-# input_city = input("Enter city name: ")
-# lat, lon = search_city(input_city)
-# current_weather = get_current_weather(lat, lon)
-# print(f"City: {current_weather[0][0]}")
-# print(f"Location: {current_weather[0][1]}, {current_weather[0][2]}")
-# print(f"Weather: {current_weather[0][4]}")
-# temperature = round(current_weather[0][6] - 273.15, 1)
-# print(f"Temperature: {temperature} °C")
-
 @app.route('/search_city', methods=['POST'])
 def search_city():
     receive = request.get_json()
